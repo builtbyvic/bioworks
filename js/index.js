66f4029caf09982a5f009d4b0f1acf2558afc6bd
vic⚡ <builtbyvic@gmail.com>
2022-03-16 17:42:18 +0300

    added responsive nav

--- js/index.js
+++ js/index.js
@@ -5,3 +5,19 @@ const links = document.querySelectorAll('.nav-links li');
 hamburger.addEventListener("click",() => {
   navLinks.classList.toggle("open");
 });
+// ham code
+const menuBtn = document.querySelector('.menu-btn'); // selecting the element with class .menu-btn
+let menuOpen = false; // if the menu is open which initially will be false because the user has not clicked yet
+menuBtn.addEventListener('click',() => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen=true;
+        // ham code
+        navLinks.classList.toggle("open");
+    }else {
+        menuBtn.classList.remove('open');
+        menuOpen=false;
+        // ham code
+        navLinks.classList.toggle("open");
+    }
+});
